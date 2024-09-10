fccd2fbbd2fb85abe958baa2df01aeab5e907f1b
Sanjay <sanjay.uppugandla@gmail.com>
2024-09-10 11:54:19 -0700

    initial project set up

---
+++ src/main/java/com/example/clientsocket/ClientSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientSocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clientsocket/ClientSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
